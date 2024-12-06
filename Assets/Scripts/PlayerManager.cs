560f1d82f2725b577b987892329b76a7ee507cba
bonjour <camille.gaudron@utbm.fr>
2024-12-06 22:52:25 +0100

    arg
    - Modelized the maze
    - Camera follows the player
    but:
    player can go through the walls

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,9 +7,10 @@ public class PlayerManager : MonoBehaviour
     private MeshRenderer meshRendererPlayer;
     private Rigidbody rbPlayer;
     private float x, y, z;
-    public float speed = 8.0f;
+    public float speed = 1.5f; // speed of the player
     private Vector3 movement;
     private GameObject gameManager;
+    public Transform cameraTransform;
 
 
     // Start is called before the first frame update
@@ -18,17 +19,44 @@ public class PlayerManager : MonoBehaviour
         meshRendererPlayer = GetComponent<MeshRenderer>();
         rbPlayer = GetComponent<Rigidbody>();
         gameManager = GameObject.Find("GameManager");
+
+        // if no camera is assigned, use the main camera
+        if (cameraTransform == null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxis("Horizontal")*speed;
+        // move the player
+        x = Input.GetAxis("Horizontal") * speed;
         y = Input.GetAxis("Jump") * speed;
-        z = Input.GetAxis("Vertical")*speed;
+        z = Input.GetAxis("Vertical") * speed;
         movement = new Vector3(x, y, z);
         rbPlayer.velocity = movement;
+
+        // move the camera
+        if (movement.magnitude > 0.1f)
+        {
+            Vector3 cameraForward = cameraTransform.forward;
+            Vector3 cameraRight = cameraTransform.right;
+
+            // ignore y axis
+            cameraForward.y = 0f;
+            cameraRight.y = 0f;
+
+            cameraForward.Normalize();
+            cameraRight.Normalize();
+
+            // final movement direction
+            Vector3 moveDirection = cameraForward * movement.z + cameraRight * movement.x;
+
+            MovePlayer(moveDirection);
+        }
     }
+        
     private void OnCollisionEnter(Collision collision)
     {
         GameObject collidedWith = collision.gameObject;
@@ -40,5 +68,20 @@ public class PlayerManager : MonoBehaviour
         {
             Destroy(collidedWith);
         }
+    }
+
+    private void MovePlayer(Vector3 direction)
+    {
+        Vector3 movement = direction * speed * Time.deltaTime;
+        rbPlayer.MovePosition(transform.position + movement);
+
+        //orientation of the player
+        if (direction != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.deltaTime);
+        }
+
+
     }
 }
