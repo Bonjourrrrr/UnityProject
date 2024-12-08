66e76947ebca3cb5c3e44515dc105a3d3825e270
bonjour <camille.gaudron@utbm.fr>
2024-12-08 16:23:38 +0100

    Level 2 with the wolf waiting, but bunny cannot be eaten (??)

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,30 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class EnemyManager : MonoBehaviour
 {
     private GameObject myEnemy, myPlayer;
     private NavMeshAgent navMeshEnemy;
+    private bool isWaiting = false;
+
     void Start()
     {
         myPlayer = GameObject.Find("Player");
-        myEnemy=this.gameObject;
+        myEnemy = this.gameObject;
         navMeshEnemy = myEnemy.GetComponent<NavMeshAgent>();
+
+        if (myEnemy.tag == "WaitingWolf" && isWaiting == false)
+        {
+            navMeshEnemy.enabled = false;
+            StartCoroutine(WaitBeforeContinue());
+            isWaiting = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        navMeshEnemy.destination = myPlayer.transform.position;
+        if (!isWaiting)
+        {
+            navMeshEnemy.destination = myPlayer.transform.position;
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Destroy(myPlayer);
+            SceneManager.LoadScene(0);
         }
+    }
+    private IEnumerator WaitBeforeContinue()
+    {
+        yield return new WaitForSeconds(20);
+        navMeshEnemy.enabled = true;
+        isWaiting = false;
     }
 }
 
