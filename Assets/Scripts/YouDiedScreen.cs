6ad671cd42c2ac753ad77e5eb35ca0b1ff19927d
bonjour <camille.gaudron@utbm.fr>
2024-12-11 18:34:41 +0100

    save of lvl 2 + when u die at lvl 4 or 5 you restart at lvl 3

--- Assets/Scripts/YouDiedScreen.cs
+++ Assets/Scripts/YouDiedScreen.cs
@@ -13,7 +13,10 @@ public class YouDiedScreen : MonoBehaviour
     {
         Debug.Log("Waiting for 2 seconds");
         yield return new WaitForSeconds(2f);
-        Debug.Log("Loading scene 0");
-        SceneManager.LoadScene(0);
+        Debug.Log("Loading scene");
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.Load();
+        }
     }
 }
